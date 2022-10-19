5e14761a6fe143ba9d467a303050474e3d44792a
Violeta759 <116067584+Violeta759@users.noreply.github.com>
2022-10-19 15:09:07 +0000

    Mi primer programa

---
+++ python/hola.py
@@ -0,0 +1,6 @@
+print("HOLA MUNDO")
+#APPV: Con f le decimos dentro de print que no son sólo carácteres sino también otras cosas.
+a=0
+print(f'La variable a tiene el valor de a es {a}')
+a=10
+print(f'El valor de a es {a}')
