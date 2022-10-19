87436fa2be440d2d6eb78c1037886cf356335eb1
Violeta759 <116067584+Violeta759@users.noreply.github.com>
2022-10-19 17:05:56 +0000

    Cosas de clase

--- python/hola.py
+++ python/hola.py
@@ -1,5 +1,5 @@
 print("HOLA MUNDO")
-#APPV: Con f le decimos dentro de print que no son sólo carácteres sino también otras cosas.
+#APPV: Con f le decimos dentro de print que no son sólo carácteres sino también otras cosas
 a=0
 print(f'La variable a tiene el valor de a es {a}')
 a=10
